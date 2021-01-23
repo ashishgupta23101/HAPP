b1730dae0b489506c1794f4cf33a3b3153f9192b
vaibhavit80 <45264740+vaibhavit80@users.noreply.github.com>
2021-01-23 19:29:52 +0530

    j

---
+++ src/app/models/MailData.ts
@@ -0,0 +1,4 @@
+export class MailData{
+    subject: string;
+    from: string;
+}
--- src/app/public/listing-retailer/listing-retailer.page.ts
+++ src/app/public/listing-retailer/listing-retailer.page.ts
@@ -1,4 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { MailData } from 'src/app/models/MailData';
 import { FunctionsService } from 'src/app/providers/functions.service';
 import { TrackingService } from 'src/app/providers/tracking.service';
 
@@ -8,31 +10,68 @@ import { TrackingService } from 'src/app/providers/tracking.service';
   styleUrls: ['./listing-retailer.page.scss'],
 })
 export class ListingRetailerPage implements OnInit {
-mailData : any;
+
+  mailData : Array<MailData>;
   constructor(                 
   @Inject(FunctionsService) public fun: FunctionsService,
-  @Inject(TrackingService) public trackService: TrackingService) {
-
-   }
-
+  @Inject(TrackingService) public trackService: TrackingService,
+  @Inject(NavController) private navCtrl: NavController,) { }
+  goBack() {
+    this.navCtrl.back();
+  }
   ngOnInit() {
+    this.mailData = [];
     this.trackService.getMessages(localStorage.getItem('accessToken')).subscribe(data => {
       // tslint:disable-next-line: no-debugger
        data.messages.forEach(element => {
          this.trackService.getMessagebyId(localStorage.getItem('accessToken'),element.id).subscribe(data => {
           // tslint:disable-next-line: no-debugger
-          this.mailData = data;
-          console.log(this.mailData);
+          if(data){
+           
+            debugger;
+            let md = new MailData(); 
+            // for (let i = 0; i < data.payload.headers.length; i++) {
+            //   let val = data.payload.headers[i];
+            //   if(val.name === 'Subject' && (val.value.toLowerCase().includes('order') 
+            //   || val.value.toLowerCase().includes('your package')
+            //   || val.value.toLowerCase().includes('your return') )){
+            //     md.subject = val.value ;
+                
+            //   }
+            // }
+            data.payload.headers.forEach(val => {
+              
+              if(val.name === 'Subject' && (val.value.toLowerCase().includes('order') 
+              || val.value.toLowerCase().includes('your package')
+              || val.value.toLowerCase().includes('your return'))){
+                md.subject = val.value;
+                md.from = 'NA';
+                this.mailData.push(md)
+              }
+            });
+          }
+          
         },
         error => {
          localStorage.setItem('IsLogin', 'false');
          this.fun.dismissLoader();
          this.fun.presentToast('Invalid Login data!', true, 'bottom', 2100);
         });
+
        });
+       console.log(this.mailData);
     },
     error => {
-     localStorage.setItem('IsLogin', 'false');
+      debugger;
+      if(error.status === 401)
+      {
+        this.fun.dismissLoader();
+        localStorage.setItem('IsLogin', 'false');
+        localStorage.setItem('accessToken', 'NA');
+        this.fun.presentToast('Unauthenticated request! Please login', true, 'bottom', 2100);
+        this.goBack();
+      }
+     
      this.fun.dismissLoader();
      this.fun.presentToast('Invalid Login data!', true, 'bottom', 2100);
     });
